9b7383b3ba3e87aeb9733645aceab0b94261e44f
Damian Kłos <alojzy231@gmail.com>
2023-05-19 21:52:57 +0200

    refactor: added addProduct mutation

---
+++ apps/frontend/src/features/Dashboard/api/mutations/useAddProduct/index.ts
@@ -0,0 +1,11 @@
+import { AddProductDTO } from '@pharmacy-crud/dto';
+import { useMutation } from '@tanstack/react-query';
+import { UseMutationResult } from '@tanstack/react-query/src/types';
+
+import { productService } from '@api/services/ProductService';
+
+const addProduct = async (addProduct: AddProductDTO) => {
+  await productService.addProduct(addProduct);
+};
+
+export const useAddProduct = (): UseMutationResult<AddProductDTO> => useMutation(addProduct);
