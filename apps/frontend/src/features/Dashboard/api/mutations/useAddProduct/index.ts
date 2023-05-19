cbda8a981f93808c5a37e6beec283a4490bd0046
Damian Kłos <alojzy231@gmail.com>
2023-05-19 23:17:28 +0200

    feat: invalidate get products query on successful mutation

--- apps/frontend/src/features/Dashboard/api/mutations/useAddProduct/index.ts
+++ apps/frontend/src/features/Dashboard/api/mutations/useAddProduct/index.ts
@@ -1,12 +1,21 @@
 import { AddProductDTO } from '@pharmacy-crud/dto';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { UseMutationResult } from '@tanstack/react-query/src/types';
 
 import { productService } from '@api/services/ProductService';
+
+import { productsKey } from '../../queries/useGetProducts';
 
 const addProduct = async (addProduct: AddProductDTO) => {
   await productService.addProduct(addProduct);
 };
 
-export const useAddProduct = (): UseMutationResult<unknown, unknown, AddProductDTO> =>
-  useMutation(addProduct);
+export const useAddProduct = (): UseMutationResult<unknown, unknown, AddProductDTO> => {
+  const queryClient = useQueryClient();
+
+  return useMutation(addProduct, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(productsKey.details());
+    },
+  });
+};
