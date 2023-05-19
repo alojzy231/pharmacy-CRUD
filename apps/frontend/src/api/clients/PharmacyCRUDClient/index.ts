228e676ae88019e79df3ea3841f4f2e719f10cf3
Damian Kłos <alojzy231@gmail.com>
2023-05-19 21:41:42 +0200

    refactor: added PharmacyCRUDClientApiClient

---
+++ apps/frontend/src/api/clients/PharmacyCRUDClient/index.ts
@@ -0,0 +1,8 @@
+import { HttpClient } from '../HttpClient';
+
+export abstract class PharmacyCRUDClientApiClient extends HttpClient {
+  protected constructor() {
+    // TODO: write config for api url
+    super(process.env.NEXT_PUBLIC_API_URL as string, '/v1');
+  }
+}
