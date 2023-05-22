20edecc749cc68aa96e21688831d75418b923cc5
Damian Kłos <alojzy231@gmail.com>
2023-05-22 22:45:18 +0200

    refactor: update Product record if data exists

--- apps/backend/src/actions/post/addProduct.ts
+++ apps/backend/src/actions/post/addProduct.ts
@@ -5,8 +5,10 @@ import { prisma } from "../../client";
 
 export async function addProduct(data: AddProductDTO) {
   try {
-    await prisma.product.create({
-      data,
+    await prisma.product.upsert({
+      create: data,
+      where: { name: data.name },
+      update: { quantity: { increment: data.quantity } },
     });
   } catch (error) {
     console.error(`Error adding product: ${error}`);
