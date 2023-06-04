57239138899fc286c038439684e55a8514b9b871
Damian Kłos <alojzy231@gmail.com>
2023-06-04 13:33:21 +0200

    fix: unused declaration of prisma client globally

--- packages/prisma-client/src/client.ts
+++ packages/prisma-client/src/client.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient =
-  global.prismaClient ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
+export const prismaClient = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
     },
-  });
-
-if (process.env.NODE_ENV !== "production") global.prismaClient = prismaClient;
+  },
+});
