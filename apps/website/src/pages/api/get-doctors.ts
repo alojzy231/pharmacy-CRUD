1d4c89db1d4493f3f398557655e7f7290d84fb84
Damian Kłos <alojzy231@gmail.com>
2023-06-08 17:53:11 +0200

    feat: added get doctors endpoint

---
+++ apps/website/src/pages/api/get-doctors.ts
@@ -0,0 +1,24 @@
+import { prismaClient } from '@config/prismaClient';
+import { GetHospitalsResultDTO } from '@dto';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function getDoctors(
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<void | NextApiResponse<GetHospitalsResultDTO>> {
+  if (request.method !== 'GET') return response.status(405).end();
+
+  try {
+    const data = await prismaClient.doctor.findMany({ include: { hospital: true } });
+
+    prismaClient.$disconnect();
+
+    return response.status(200).json({ data });
+  } catch (error) {
+    prismaClient.$disconnect();
+
+    console.error(`Error getting all doctors: ${error}`);
+
+    return response.status(500).json({ error, message: 'Error getting all doctors' });
+  }
+}
