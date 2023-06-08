3dd3e7dc91ab89e43e74e89aa5fa4234eb80e57a
Damian Kłos <alojzy231@gmail.com>
2023-06-08 19:12:02 +0200

    fix: missing prisma migration

---
+++ apps/website/prisma/migrations/20230608161755_removed_address_from_doctor_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `address` on the `Doctor` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Doctor` DROP COLUMN `address`;
