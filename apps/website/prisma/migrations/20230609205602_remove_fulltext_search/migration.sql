2053e1c4008757856123dcc414903340134af1a9
Damian Kłos <alojzy231@gmail.com>
2023-06-09 22:57:04 +0200

    refactor: remove fulltext search from prisma

---
+++ apps/website/prisma/migrations/20230609205602_remove_fulltext_search/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX `Doctor_lastName_idx` ON `Doctor`;
+
+-- DropIndex
+DROP INDEX `Doctor_name_idx` ON `Doctor`;
+
+-- DropIndex
+DROP INDEX `Hospital_city_idx` ON `Hospital`;
+
+-- DropIndex
+DROP INDEX `Hospital_name_idx` ON `Hospital`;
+
+-- DropIndex
+DROP INDEX `Product_name_idx` ON `Product`;
