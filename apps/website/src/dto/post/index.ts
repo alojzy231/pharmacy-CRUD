91fc3ed3d5403c97146094a89dd4d3763cb130b3
Damian Kłos <alojzy231@gmail.com>
2023-06-10 14:02:43 +0200

    feat: added login endpoint

--- apps/website/src/dto/post/index.ts
+++ apps/website/src/dto/post/index.ts
@@ -1,3 +1,4 @@
 export * from './addProduct.types';
 export * from './addHospital.types';
 export * from './addDoctor.types';
+export * from './login.types';
---
+++ apps/website/src/pages/api/login.ts
@@ -0,0 +1,52 @@
+import { prismaClient } from '@config/prismaClient';
+import { LoginArgumentsDTO } from '@dto';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { setCookie } from 'nookies';
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
+
+function signAccessToken(payload: LoginArgumentsDTO): Promise<string> {
+  return new Promise((resolve, reject) => {
+    jwt.sign({ payload }, ACCESS_TOKEN_SECRET, {}, (error, token) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(token as string);
+    });
+  });
+}
+
+export default async function login(
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<NextApiResponse> {
+  if (request.method !== 'POST') return response.status(405).end();
+
+  const { email, password }: LoginArgumentsDTO = request.body;
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return response.status(401).end();
+  }
+
+  const checkPassword = bcrypt.compareSync(password, user.password);
+
+  if (!checkPassword) {
+    return response.status(401).end();
+  }
+  const accessToken = await signAccessToken(user);
+
+  setCookie({ res: response }, 'accessToken', accessToken, {
+    maxAge: 24 * 60 * 60, // 1 day
+    path: '/',
+  });
+
+  return response.status(200).end();
+}
